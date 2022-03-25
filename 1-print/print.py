ef542c9c1799ed8298324285088ad2810cd89e3b
William Martins <72496595+WilliamKotoco@users.noreply.github.com>
2022-03-24 21:08:30 -0300

    funcoes do print

---
+++ 1-print/print.py
@@ -0,0 +1,7 @@
+print("carlos", "josé", sep="-")
+# sep vai definir um separador entre os dois argumentos de
+print("carlos", "josé", sep="-", end="")
+print("maria")
+# o end define algo na linha final. Por padrão ele dá uma quebra de linha]
+
+
---
+++ 1-print/separador-cpf.py
@@ -0,0 +1,4 @@
+# Mostre o seguinte CPF de forma separada usando a função print: 66682557019
+
+print("666", "825", "570", sep=".", end="-")
+print("19")  # dígito
